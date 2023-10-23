60e0059ac5e08a9ad397731a942086255bceec37
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2023-10-23 19:45:58 -0300

    fizz logic and refactor when converting integers to string

--- FizzBuzzKata/FizzBuzz.Tests/FizzBuzzShould.cs
+++ FizzBuzzKata/FizzBuzz.Tests/FizzBuzzShould.cs
@@ -5,14 +5,18 @@ namespace FizzBuzz.Tests;
 public class FizzBuzzShould
 {
     [TestMethod]
-    public void ConvertOneToOne()
+    public void ConvertIntegerToString()
     {
         Assert.AreEqual("1", FizzBuzz.Convert(1));
         Assert.AreEqual("2", FizzBuzz.Convert(2));
-        Assert.AreEqual("Fizz", FizzBuzz.Convert(3));
-
+        Assert.AreEqual("4", FizzBuzz.Convert(4));
 
     }
+    [TestMethod]
+    public void ConvertThreeToFizz()
+    {
+        Assert.AreEqual("Fizz", FizzBuzz.Convert(3));
 
+    }
 
 }
--- FizzBuzzKata/FizzBuzz/FizzBuzz.cs
+++ FizzBuzzKata/FizzBuzz/FizzBuzz.cs
@@ -4,12 +4,8 @@ public static class FizzBuzz
 {
     public static string Convert(int number)
     {
-        if (number == 1)
-            return "1";
-        if (number == 2)
-            return "2";
         if (number == 3)
             return "Fizz";
-        return "4";
+        return number.ToString();
     }
 }
