c2c0f97d813f7f19c105e013d9048d85140bdd13
Carlos Leuck Moreira <carlinhosrlz@gmail.com>
2023-10-30 18:26:17 -0300

    test passed with hard code

--- AtmKata/Atm.Tests/AtmTest.cs
+++ AtmKata/Atm.Tests/AtmTest.cs
@@ -4,8 +4,11 @@ namespace Atm.Tests
     public class AtmTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldReturnCorrectlyAmountWhenHaveMoney()
         {
+            Atm atm = new();
+            Assert.AreEqual(@"2 bills of 200.1 bill of 20.1 bill of 10.2 coins of 2.", atm.Withdraw(434));
+
         }
     }
 }
--- AtmKata/Atm/Atm.cs
+++ AtmKata/Atm/Atm.cs
@@ -1,7 +1,11 @@
-﻿namespace Atm
+﻿
+namespace Atm
 {
     public class Atm
     {
-
+        public string Withdraw(int quantity)
+        {
+            return @"2 bills of 200.1 bill of 20.1 bill of 10.2 coins of 2.";
+        }
     }
 }
